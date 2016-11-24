eaa30bd7904384e17a912bf20ab7999134bcbb46
derekja <derekja@gmail.com>
2016-11-23 16:33:33 -0800

    initial commit of clearDB

--- public/index.js
+++ public/index.js
@@ -1,18 +1,4 @@
-//Test for browser compatibility
-if (window.openDatabase) {
-    //Create the database the parameters are 1. the database name 2.version number 3. a description 4. the size of the database (in bytes) 1024 x 1024 = 1MB
-    var mydb = openDatabase("cars_db", "0.1", "A Database of Cars I Like", 1024 * 1024);
 
-    //create the cars table using SQL for the database using a transaction
-    mydb.transaction(function(t) {
-        t.executeSql("CREATE TABLE IF NOT EXISTS cars (id INTEGER PRIMARY KEY ASC, make TEXT, model TEXT)");
-    });
-
-
-
-} else {
-    alert("WebSQL is not supported by your browser!");
-}
 
 //function to output the list of cars in the database
 
@@ -41,55 +27,66 @@ function updateCarList(transaction, results) {
 //function to get the list of cars from the database
 
 function outputCars() {
-    //check to ensure the mydb object has been created
-    if (mydb) {
-        //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
-        mydb.transaction(function(t) {
-            t.executeSql("SELECT * FROM cars", [], updateCarList);
-        });
-    } else {
-        alert("db not found, your browser does not support web sql!");
-    }
+    //get the current list of cars as a JSON object
+
+        var response = null;
+       var xmlhttp = new XMLHttpRequest();
+        var reqStr = "/carList";
+        xmlhttp.open("GET",reqStr,true);
+
+      xmlhttp.onreadystatechange=function() {
+            if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+                response = xmlhttp.responseText; //if you need to do something with the returned value
+                console.log(response);
+            }
+        }
+
+
+        xmlhttp.send();
+
 }
 
 //function to add the car to the database
 
 function addCar() {
-    //check to ensure the mydb object has been created
-    if (mydb) {
+
         //get the values of the make and model text inputs
         var make = document.getElementById("carmake").value;
         var model = document.getElementById("carmodel").value;
 
-        //Test to ensure that the user has entered both a make and model
-        if (make !== "" && model !== "") {
-            //Insert the user entered details into the cars table, note the use of the ? placeholder, these will replaced by the data passed in as an array as the second parameter
-            mydb.transaction(function(t) {
-                t.executeSql("INSERT INTO cars (make, model) VALUES (?, ?)", [make, model]);
-                outputCars();
-            });
-        } else {
-            alert("You must enter a make and model!");
+        var xmlhttp = new XMLHttpRequest();
+        var reqStr = "/add?make="+make+"&model="+model;
+        xmlhttp.onreadystatechange=function() {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            var response = xmlhttp.responseText; //if you need to do something with the returned value
         }
-    } else {
-        alert("db not found, your browser does not support web sql!");
-    }
+        }
+
+        xmlhttp.open("GET",reqStr,true);
+        xmlhttp.send();
+
+        
+        setTimeout(outputCars, 2000);
+ 
 }
 
 
 //function to remove a car from the database, passed the row id as it's only parameter
 
 function deleteCar(id) {
-    //check to ensure the mydb object has been created
-    if (mydb) {
-        //Get all the cars from the database with a select statement, set outputCarList as the callback function for the executeSql command
-        mydb.transaction(function(t) {
-            t.executeSql("DELETE FROM cars WHERE id=?", [id], outputCars);
-        });
-    } else {
-        alert("db not found, your browser does not support web sql!");
-    }
+
+    var xmlhttp = new XMLHttpRequest();
+    var reqStr = "/remove?id="+id;
+xmlhttp.onreadystatechange=function() {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+    var response = xmlhttp.responseText; //if you need to do something with the returned value
+  }
+}
+
+xmlhttp.open("GET",reqStr,true);
+xmlhttp.send();
+
+setTimeout(outputCars, 2000);
 }
 
 outputCars();
-    
--- server.js
+++ server.js
@@ -3,13 +3,91 @@ var express = require("express"),
 
 var port = process.env.VCAP_APP_PORT || 8080;
 
+function getList(queryStr, callback) {
+
+
+    clearDBconnection.query(queryStr, function(err, rows, fields) {
+    if (err) throw err;
+    callback(rows)
+    });
+
+}
+
+var mysql      = require('mysql');
+var clearDBconnection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-04.cleardb.net',
+  user     : 'bffc7e639d3ebd',
+  password : '592aacc4',
+  database : 'ad_08d31853cfcf804'
+});
+
+setInterval(function () {
+    clearDBconnection.query('SELECT 1');
+}, 5000);
+
 app.use(express.static(__dirname + '/public'));
 
-app.get("/hello", function (request, response) {
+
+app.get("/carList", function(request, response){
+    var id = request.query.id;
+    var queryStr = "SELECT * from cars";
+    var rowsResponse = null;
+
     response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World!\n");
-});
+    console.log(queryStr);
+    
+    getList(queryStr, function(rows) {
+
+
+        var objs = [];
+        for (var i = 0;i < rows.length; i++) {
+          objs.push({key: rows[i].key, name: rows[i].name, model: rows[i].model});
+        }
+
+
+        rowsResponse = JSON.stringify(objs);
+        console.log(rowsResponse);
+
+        response.end(JSON.stringify(objs)); 
+    })
+
+
+})
+
+app.get("/add", function(request, response){
+    var id = null;
+    var make = request.query.make;
+    var model = request.query.model;
+    var queryStr = "INSERT INTO cars (cars.key,cars.name,cars.model) VALUES ('"+id+"','"+make+"','"+model+"')";
+
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    console.log(queryStr);
+
+    clearDBconnection.query(queryStr, function(err, rows, fields) {
+    if (err) throw err;
+    });
+
+
+
+    response.end("deleted!\n");
+})
+
+app.get("/remove", function(request, response){
+    var id = request.query.id;
+    var queryStr = "DELETE from cars WHERE cars.key = "+id;
+
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    console.log(queryStr);
+
+    clearDBconnection.query(queryStr, function(err, rows, fields) {
+    if (err) throw err;
+    });
+
+
+    response.end("deleted!\n");
+})
 
 app.listen(port);
 
 require("cf-deployment-tracker-client").track();
+
